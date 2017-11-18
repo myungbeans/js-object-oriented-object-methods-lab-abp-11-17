a94bca2a929f1f9f7a1c1ba338c8b9b3732aa727
Michael Myung <michael.h.myung@gmail.com>
2017-11-18 15:46:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,60 @@
-function BoardMember(name, homeState, training){
-  this.name = name;
-  this.homeState = homeState;
-  this.training = training;
+function BoardMember(name, homeState, training) {
+  this.name = name
+  this.homeState = homeState
+  this.training = training
   this.veto = function(){
-    return "No, I must disagree"};
+    return "No, I must disagree"}
   this.approve = function(){
-    return "You can do that!";
+    return "You can do that!"
   }
   this.doCharity = function(){
-    return "I like to help people.";
+    return "I like to help people."
   }
   this.releasePressStatement = function(){
-    return "You will see great things from Scuber.";
+    return 'You will see great things from Scuber.'
   }
   this.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function BoardMember(name, homeState, training){
+//   this.name = name;
+//   this.homeState = homeState;
+//   this.training = training;
+//   this.veto = function(){
+//     return "No, I must disagree"};
+//   this.approve = function(){
+//     return "You can do that!";
+//   }
+//   this.doCharity = function(){
+//     return "I like to help people.";
+//   }
+//   this.releasePressStatement = function(){
+//     return "You will see great things from Scuber.";
+//   }
+//   this.sayHi = function(){
+//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+//   }
+// }
 
 //veto — returns No, I must disagree
 //approve — returns You can do that!
